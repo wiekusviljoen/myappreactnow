094967f05097f56fe6e596490fe56a47773cd7ef
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-17 21:09:04 +0200

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Home from "./pages/Home";
+import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Home />
+
+      <Pages />
     </div>
   );
 }
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Home from "./Home";
+
+function Pages() {
+  return <Home />;
+}
+export default Pages;
