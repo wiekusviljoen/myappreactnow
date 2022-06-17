1cac1efdd148c8ef6465545fb2ff5742f7802846
wiekusviljoen <wiekusviljoen@gmail.com>
2022-06-17 21:50:29 +0200

    commit on master

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,20 @@
+import { useState, useEffect } from "react";
+
 function Popular() {
-  return <div>Popular</div>;
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.React_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
+  return <div>{popular}</div>;
 }
 
 export default Popular;
